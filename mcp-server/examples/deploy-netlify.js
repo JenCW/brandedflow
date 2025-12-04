43cf89e9387b16af0741b3521f658f77882d1f50
jencortezwalters <jencortezwalters@gmail.com>
2025-12-03 18:36:19 -0800

    CLEANED UP THE FILE STRUCTURE

--- mcp-server/examples/deploy-netlify.js
+++
@@ -1,8 +0,0 @@
-// Example automation: deploy-netlify
-module.exports = async function ({ adapters, params }) {
-  const { netlify } = adapters;
-  const { siteId } = params || {};
-  if (!siteId) throw new Error('siteId required in params');
-  const result = await netlify.triggerDeploy(siteId);
-  return { deploy: result };
-};
--- mcp-server/examples/index.js
+++
@@ -1,7 +0,0 @@
-const bumpDeps = require('./bump-deps');
-const deployNetlify = require('./deploy-netlify');
-
-module.exports = {
-  'bump-deps': bumpDeps,
-  'deploy-netlify': deployNetlify
-};
